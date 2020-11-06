e24d27c5dbd5829744765be9de31554ad2a6dbc9
Rogerio <Rogerios.lucon@gmail.com>
2020-11-06 15:30:10 -0300

    Removido alguns prints.

--- GameEven/GameEven/GameViewController.swift
+++ GameEven/GameEven/GameViewController.swift
@@ -18,7 +18,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("View Did Load")
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
